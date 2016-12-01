782be5a4d97e4780c5efdbf5a1811f09951b4c2a
Michael Napolitano <mnapolit@cisco.com>
2016-12-01 14:44:55 -0500

    Initial Commit

---
+++ Trix/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Trix
+//
+//  Created by mnapolit on 12/1/16.
+//  Copyright © 2016 Micmoo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Trix/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trix
+//
+//  Created by mnapolit on 12/1/16.
+//  Copyright © 2016 Micmoo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
